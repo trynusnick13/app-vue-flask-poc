b2348b5c5ad950c8ff97ddc4f6279440d58d958f
Nikita Trynus <trynuspoc@gmail.com>
2021-05-03 23:03:03 +0300

    updated package.json

--- app/config.py
+++ app/config.py
@@ -5,8 +5,9 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:testing@localhost/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://vbfbvasilnfmdk:546a5d29343ddf6c01ac0032ad5aecda9568d7cd0e6b68d10e0f9ff15f7871c1@ec2-18-214-195-34.compute-1.amazonaws.com:5432/d34a854l5lrfq7"
     APP_DIR = os.path.dirname(__file__)
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = os.environ
     # SECRET_KEY = 'very secret key'
 
